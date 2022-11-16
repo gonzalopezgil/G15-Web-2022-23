42feb5f20b04e74a816b05447633c878da2a1173
Ekk001 <73540438+nicolaemolnar@users.noreply.github.com>
2022-11-16 23:17:05 +0100

    Spring Boot INIT
    Carga inicial del framework Spring Boot para el servidor backend

---
+++ backend/src/main/java/com/uah/gestion_de_practicas/GestionDePracticasApplication.java
@@ -0,0 +1,13 @@
+package com.uah.gestion_de_practicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDePracticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDePracticasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/uah/gestion_de_practicas/GestionDePracticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uah.gestion_de_practicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDePracticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
