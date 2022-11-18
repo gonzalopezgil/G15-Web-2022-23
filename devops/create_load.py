ff5099cdda16d4c7cb009a03b00ce178b6354f54
nicolaemolnar <nicolae.molnar@edu.uah.es>
2022-11-18 17:05:57 +0000

    SQL load script updated
    Two python scripts used to both generate data and insert it into the DB. Pending for test against real DB.

--- devops/create_load.py
+++ devops/create_load.py
@@ -10,7 +10,7 @@ def insert_supervisor(supervisor):
     return sql_template.format(supervisor['start_date'], supervisor['end_date'] if supervisor['end_date'] is not None else 'NULL')
     
 def insert_student(student):
-    sql_template = "INSERT INTO alumnos(grado, nota_exp, f_nacimiento, telefono, horas_totales) VALUES ('{}', '{}', '{}', '{}', '{}');\n"
+    sql_template = "INSERT INTO alumnos(id, grado, nota_exp, f_nacimiento, telefono, horas_totales) VALUES ('{}', '{}', '{}', '{}', '{}');\n"
     return sql_template.format(student['university_degree'], student['expedient_grade'], student['birth_date'], student['phone'], student['total_hours'])
 
 def insert_company(company):
@@ -20,6 +20,18 @@ def insert_company(company):
 def insert_tutor(tutor):
     sql_template = "INSERT INTO tutores (f_alta, f_baja,id_empresa) VALUES ('{}', '{}','{}');\n"
     return sql_template.format(tutor['start_date'], tutor['end_date'] if tutor['end_date'] is not None else 'NULL',tutor['company_id'])
+
+def insert_offer(offer):
+    sql_template = "INSERT INTO ofertas (id, puesto, categoria, id_empresa, direccion, requisitos, descripcion, horario, semanas, sueldo, plazas) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');\n"
+    return sql_template.format(offer['id'], offer['title'], offer['category'], offer['company_id'], offer['address'], offer['requirements'], offer['description'], offer['schedule'], offer['weeks'], offer['salary'], offer['vacants'])
+
+def insert_applications(application):
+    sql_template = "INSERT INTO oferta_seleccionada (id_alumno, id_oferta, preferencia) VALUES ('{}', '{}', '{}');\n"
+    return sql_template.format(application['student_id'], application['offer_id'], application['preference'])
+
+def insert_contracts(contracts):
+    sql_template = "INSERT INTO practica(id_alumno, id_oferta, nota, informe, f_inicio, f_fin) VALUES ('{}', '{}', '{}', '{}', '{}', '{}');\n"
+    return sql_template.format(contracts['student_id'], contracts['offer_id'], contracts['grade'], contracts['report'], contracts['start_date'], contracts['end_date'] if contracts['end_date'] is not None else 'NULL')
 
 def create_sql(file_name, fake, n_sep):
     # Create a SQL file to load the data
@@ -47,6 +59,24 @@ def create_sql(file_name, fake, n_sep):
         file.write("--"*n_sep + "TUTORES" + "--"*n_sep + "\n")
         for tutor in tutors:
             file.write(insert_user(tutor) + insert_tutor(tutor) + "\n")
+
+        # Insert offer data
+        offers = data_generator.generate_offers(fake, 5, companies)
+        file.write("--"*n_sep + "OFERTAS" + "--"*n_sep + "\n")
+        for offer in offers:
+            file.write(insert_offer(offer) + "\n")
+        
+        # Insert applications data
+        applications = data_generator.generate_applications(fake, 5, students, offers)
+        file.write("--"*n_sep + "OFERTA_SELECCIONADA" + "--"*n_sep + "\n")
+        for application in applications:
+            file.write(insert_applications(application) + "\n")
+
+        # Insert contracts data
+        contracts = data_generator.generate_contracts(fake, 5, applications, students)
+        file.write("--"*n_sep + "PRACTICA" + "--"*n_sep + "\n")
+        for contract in contracts:
+            file.write(insert_contracts(contract) + "\n")
     print("SQL file created")
 
 
--- devops/data_generator.py
+++ devops/data_generator.py
@@ -1,6 +1,6 @@
 # Generador de datos para la carga inicial de la base de datos
 from faker import Faker
-from random import choice, randrange
+from random import *
 from datetime import datetime
 
 # Generador de alumnos
@@ -25,6 +25,7 @@ def generate_students(fake, n):
     for i in range(n):
         student = generate_user(fake)
         # Campos específicos para alumnos
+        student['id'] = i
         student['email'] = student['username'] + '@edu.uah.es'
         student['birth_date'] = fake.date_of_birth(None, 18, 25).strftime('%Y-%m-%d')
         student['phone'] = fake.phone_number()
@@ -56,7 +57,7 @@ def generate_companies(fake, n):
 
 def get_company_without_active_tutor(companies,tutors):
     for company in companies:
-        # Check if any tutor in the company has end_date = None
+        # Comprobar si la compañía tiene un tutor activo
         if not any(tutors[tutor]['end_date'] is None for tutor in company['tutor_ids']):
             return company
     return None
@@ -99,9 +100,11 @@ def generate_tutors(fake, n, companies):
 def generate_supervisor(fake, active):
     supervisor = generate_user(fake)
     start_date = fake.date_between(start_date='-2y', end_date='today')
+
     # Campos específicos para tutores
     supervisor['email'] = supervisor['username'] + '@uah.es'
     supervisor['start_date'] = start_date.strftime('%Y-%m-%d')
+
     # Añadir fecha de fin de contrato o nulo si no ha finalizado
     supervisor['end_date'] = fake.date_between(start_date=start_date, end_date='today').strftime('%Y-%m-%d') if not active else None 
     return supervisor
@@ -122,9 +125,78 @@ def generate_supervisors(fake, n):
 
     return supervisors
 
-def generate_offer(fake, n):
-    pass
+def generate_offers(fake, n, companies):
+    offers = []
+    categories = ["Software Developer", "Data Scientist", "Data Engineer", "Machine Learning Engineer", "Data Analyst", "Data Architect", "Business Intelligence Analyst", "Data Visualization Engineer", "Data Quality Analyst", "Data Security Analyst", "Data Warehouse Engineer", "Database Administrator", "Database Architect", "Database Developer", "Database Manager", "Database Security Administrator", "Database Systems Administrator", "Database Systems Developer", "Database Systems Engineer"]
+    
+    for i in range(n):
+        company = choice(companies)
+        offer = {
+            'id': i,
+            'title': fake.job(),
+            'category': choice(categories),
+            'company_id': company['id'],
+            'address': company['address'],
+            'requirements': fake.text(),
+            'description': fake.text(),
+            'schedule': "{}-{} 5 days a week".format(randrange(8, 10), randrange(18, 20)),
+            'weeks': randrange(4, 12),
+            'salary': randrange(300, 1200),
+            'vacants': randrange(1, 10)
+        }
+        offers.append(offer)
+    return offers
 
+def generate_applications(fake, n, students, offers):
+    # Cada estudiante puede aplicar a un máximo de 10 ofertas, con una preferencia
+    applications = []
+    for student in students:
+        student['offer_ids'] = []
+        available_offers = offers.copy()
+        i = 0
+        while i < randint(1,10) and len(available_offers) > 0:
+            offer = choice(available_offers)
+            student['offer_ids'].append(offer['id'])
+            available_offers.remove(offer)
+            application = {
+                'student_id': student['id'],
+                'offer_id': offer['id'],
+                'preference': i+1
+            }
+            applications.append(application)
+            i += 1
+    return applications
+
+def generate_contracts(fake, n, applications, students):
+    contracts = []
+    users_applied_to_offer = {}
+    
+    # Collect all the users that applied to each offer 
+    for application in applications:
+        if application['offer_id'] not in users_applied_to_offer:
+            users_applied_to_offer[application['offer_id']] = []
+        users_applied_to_offer[application['offer_id']].append(application['student_id'])
+    
+    # Generate contracts for each offer
+    for offer_id in users_applied_to_offer:
+        # Sort the users by expedient_grade
+        users_applied_to_offer[offer_id].sort(key=lambda x: students[x]['expedient_grade'], reverse=True)
+
+        # Generate contracts until offers are full or there are no more applicants
+        while(len(users_applied_to_offer[offer_id]) > 0 and len(contracts) < n):
+            student_id = users_applied_to_offer[offer_id].pop(0)
+            start_date = fake.date_between(start_date='-2y', end_date='today')
+            contract = {
+                'student_id': student_id,
+                'offer_id': offer_id,
+                'grade': randrange(0, 100,1)/10,
+                'report': fake.text(),
+                'start_date': start_date.strftime('%Y-%m-%d'),
+                # TODO: Arreglar fecha de fin de contrato
+                'end_date': fake.date_between(start_date=start_date, end_date='+0y3m').strftime('%Y-%m-%d') if random() > .5 else None
+            }
+            contracts.append(contract)
+    return contracts
 
 if __name__ == '__main__':
     fake = Faker("es_ES")
@@ -132,8 +204,14 @@ if __name__ == '__main__':
     companies = generate_companies(fake, 2)
     tutors = generate_tutors(fake, 5, companies)
     supervisors = generate_supervisors(fake, 3)
+    offers = generate_offers(fake, 5, companies)
+    applications = generate_applications(fake, 5, students, offers)
+    contracts = generate_contracts(fake, 5, applications, students)
 
     print(f"Alumnos: \n{students}")
     print(f"Empresas: \n{companies}")
     print(f"Tutores: \n{tutors}")
     print(f"Supervisores: \n{supervisors}")
+    print(f"Ofertas: \n{offers}")
+    print(f"Solicitudes: \n{applications}")
+    print(f"Prácticas: \n{contracts}")
