22c33e280675d17616d49deeb17daf71ecd6bff7
Nicolae Alexandru Molnar <73540438+nicolaemolnar@users.noreply.github.com>
2023-01-02 11:23:40 +0100

    Revert "Revert "Front homeuserscomponent""

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,71 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardUsersComponent } from './pages/dashboard-users/dashboard-users.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { AuthGuard } from './guards/auth.guard';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PracticesComponent } from './components/users/practices/practices.component';
+import { ReportsComponent } from './components/users/reports/reports.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { PracticesOngoingComponent } from './components/users/practices-ongoing/practices-ongoing.component';
+import { UserdataComponent } from './components/users/userdata/userdata.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomePageComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent
+  },
+  {
+    path: 'dashboard-users',
+    component: DashboardUsersComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'welcome',
+        component: WelcomeComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'practices',
+        component: PracticesComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'reports',
+        component: ReportsComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'practicesongoing',
+        component: PracticesOngoingComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'userdata',
+        component: UserdataComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: '',
+        redirectTo: 'welcome',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/interfaces/practica.ts
@@ -0,0 +1,10 @@
+export interface Practica {
+    id: number;
+    posicion: number;
+    titulo: string;
+    empresa: string;
+    plazas: number;
+    horario: string;
+    dias: string;
+    semanas: number;
+  }
