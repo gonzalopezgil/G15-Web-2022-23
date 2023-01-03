dc53051c6782b26dc9d8608a97d73e5827378806
Gonzalo López Gil <gonzalo.lopezg@edu.uah.es>
2023-01-03 23:49:28 +0100

    Company controller commented for the Swagger API

--- backend/src/main/java/com/uah/gestion_de_practicas/controller/CompanyController.java
+++ backend/src/main/java/com/uah/gestion_de_practicas/controller/CompanyController.java
@@ -2,6 +2,7 @@ package com.uah.gestion_de_practicas.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,9 @@ import com.uah.gestion_de_practicas.model.Practice;
 import com.uah.gestion_de_practicas.model.Supervisor;
 import com.uah.gestion_de_practicas.model.Tutor;
 import com.uah.gestion_de_practicas.service.CompanyService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 /**
  * Rest Controller for the Company endpoint
@@ -39,9 +43,13 @@ public class CompanyController {
      * @return the registered company in the body of the response
      */
     @PostMapping("")
-    public ResponseEntity<Company> registerCompany(@RequestBody Company company, @RequestBody Tutor tutor) {
+    @ApiOperation("Registration of a new company by a tutor")
+    public ResponseEntity<Company> registerCompany(@ApiParam("The company to be registered") @RequestBody Company company, @ApiParam("The tutor who is registering the company") @RequestBody Tutor tutor) {
         if (company == null || tutor == null || company.getId() != null || tutor.getCompany() != null) {
             return ResponseEntity.badRequest().build();
+        }
+        if (tutor.getCompany() != null) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
 
         Company registered_company = companyService.saveCompany(company);
@@ -55,7 +63,8 @@ public class CompanyController {
      * @return the list of companies with their students in the body of the response
      */
     @GetMapping("")
-    public ResponseEntity<List<CompanyStudentsDTO>> getAllCompaniesWithStudents(@RequestBody Supervisor supervisor) {
+    @ApiOperation("The supervisor gets an overview of the companies with the number of students doing their practices in each one")
+    public ResponseEntity<List<CompanyStudentsDTO>> getAllCompaniesWithStudents(@ApiParam("The supervisor who is requesting the list") @RequestBody Supervisor supervisor) {
         if (supervisor == null) {
             return ResponseEntity.badRequest().build();
         }
@@ -70,7 +79,8 @@ public class CompanyController {
      * @return the company information in the body of the response
      */
     @GetMapping("/{id}")
-    public ResponseEntity<Company> getCompanyById(@PathVariable(name = "id") Long id) {
+    @ApiOperation("Obtaining the information of a company")
+    public ResponseEntity<Company> getCompanyById(@ApiParam("The id of the company") @PathVariable(name = "id") Long id) {
         if (id == null) {
             return ResponseEntity.badRequest().build();
         }
@@ -88,7 +98,8 @@ public class CompanyController {
      * @return the modified company in the body of the response
      */
     @PostMapping("/{id}")
-    public ResponseEntity<Company> modifyCompany(@PathVariable(name = "id") Long id, @RequestBody Company company) {
+    @ApiOperation("Modifying the information of a company")
+    public ResponseEntity<Company> modifyCompany(@ApiParam("The id of the company") @PathVariable(name = "id") Long id, @ApiParam("The company object with the new information") @RequestBody Company company) {
         if (id == null || company == null) {
             return ResponseEntity.badRequest().build();
         }
@@ -106,11 +117,12 @@ public class CompanyController {
      * Endpoint to obtain the list of actual students in a company
      * Only the tutor of the company can obtain this list
      * @param id, the id of the company
+     * @param tutor, the tutor of the company
      * @return the list of students in the body of the response
      */
     @GetMapping("/{id}/students")
-    public ResponseEntity<List<StudentDTO>> getStudentsInCompany(@PathVariable(name = "id") Long id,
-            @RequestBody Tutor tutor) {
+    @ApiOperation("The tutor of a company gets the list of actual students in the company")
+    public ResponseEntity<List<StudentDTO>> getStudentsInCompany(@ApiParam("The id of the company") @PathVariable(name = "id") Long id, @ApiParam("The tutor of the company") @RequestBody Tutor tutor) {
         if (id == null || tutor == null || tutor.getCompany().getId() != id || tutor.getRemoval_date() != null) {
             return ResponseEntity.badRequest().build();
         }
@@ -125,11 +137,12 @@ public class CompanyController {
      * Endpoint to publish the reports of the students in a company
      * Only the tutor of the company can publish the reports
      * @param id, the id of the company
-     * @param tutor, the tutor who is publishing the reports
+     * @param practices, the list of practices with the reports to be published
      * @return the list of reports of the students in the body of the response
      */
     @PostMapping("/{id}/practice")
-    public ResponseEntity<List<Practice>> publishReports(@PathVariable(name = "id") Long id, @RequestBody List<Practice> practices) {
+    @ApiOperation("The tutor of a company publishes the reports of the students in the company")
+    public ResponseEntity<List<Practice>> publishReports(@ApiParam("The id of the company") @PathVariable(name = "id") Long id, @ApiParam("The list of practices with the reports to be published") @RequestBody List<Practice> practices) {
         if (id == null || practices == null) {
             return ResponseEntity.badRequest().build();
         }
@@ -148,8 +161,8 @@ public class CompanyController {
      * @return the list of practices in the body of the response
      */
     @GetMapping("/{id}/practice")
-    public ResponseEntity<List<Practice>> getPracticeHistory(@PathVariable(name = "id") Long id,
-            @RequestBody Tutor tutor) {
+    @ApiOperation("The tutor of a company gets the history of practices of the company, completed or in progress")
+    public ResponseEntity<List<Practice>> getPracticeHistory(@ApiParam("The id of the company") @PathVariable(name = "id") Long id, @ApiParam("The tutor who is requesting the history") @RequestBody Tutor tutor) {
         if (id == null || tutor == null || tutor.getCompany().getId() != id || tutor.getRemoval_date() != null) {
             return ResponseEntity.badRequest().build();
         }
