878b5378a13eae43fe15fc6db96d0edb8a8a9fe7
Carlos L <clg2000.cl@gmail.com>
2023-01-07 13:10:39 +0000

    responsabledata finished

--- frontend/src/app/components/responsable/responsabledata/responsabledata.component.ts
+++ frontend/src/app/components/responsable/responsabledata/responsabledata.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Responsable } from 'src/app/interfaces/responsable';
-import { UsersService } from 'src/app/services/users.service';
+import { responsable } from 'src/app/interfaces/responsable';
+import { ResponsableService } from 'src/app/services/responsable.service';
 import { PopUpPasswordComponent } from '../../pop-ups/pop-up-password/pop-up-password.component';
+
 @Component({
   selector: 'app-responsabledata',
   templateUrl: './responsabledata.component.html',
   styleUrls: ['./responsabledata.component.scss']
 })
+
 export class ResponsabledataComponent {
-  responsable: Responsable | undefined;
-  constructor(private usersService: UsersService,private dialog: MatDialog) { }
+  responsable: responsable | undefined;
+  constructor(private responsableService: ResponsableService,private dialog: MatDialog) { }
 
   ngOnInit(): void {
-    this.responsable = this.usersService.getRespnsable();
+    this.responsable = this.responsableService.getResponsable();
   }
 
   changePassword() {
