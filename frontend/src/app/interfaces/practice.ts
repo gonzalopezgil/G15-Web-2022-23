3ce7d8f5271c951575b3eed679a9c28f02c99a41
rubenminero <rubenminero12@gmail.com>
2023-01-10 19:59:44 +0100

    Revert "added offer interface"
    This reverts commit 213bff9bb985968a10496cedda415ee595d70f4f.

--- frontend/src/app/interfaces/practice.ts
+++
@@ -1,9 +0,0 @@
-export interface Practice {
-    id: number,
-    offer_id: number,
-    student_id: number
-    mark: number,
-    report: string,
-    start_date: string,
-    end_date: string,
-}
